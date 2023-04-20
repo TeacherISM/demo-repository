1532b87d1e7fea2628d96196efeec7cad0ef26a2
Ivan Santiago <ivsanmendez@gmail.com>
2023-04-20 14:45:06 -0600

    Add coverage dependecy

--- python3/src/app.py
+++ python3/src/app.py
@@ -4,14 +4,17 @@ from flask import Flask, render_template, request, jsonify
 # create the application object
 app = Flask(__name__)
 
+
 # use decorators to link the function to a url
 @app.route('/')
 def home():
     return "Hello, World!"  # return a string
 
+
 @app.route('/welcome')
 def welcome():
     return render_template('welcome.html')  # render a template
+
 
 countries = [
     {"id": 1, "name": "Thailand", "capital": "Bangkok", "area": 513120},
@@ -19,12 +22,15 @@ countries = [
     {"id": 3, "name": "Egypt", "capital": "Cairo", "area": 1010408},
 ]
 
+
 def _find_next_id():
     return max(country["id"] for country in countries) + 1
+
 
 @app.get("/countries")
 def get_countries():
     return jsonify(countries)
+
 
 @app.post("/countries")
 def add_country():
@@ -34,6 +40,7 @@ def add_country():
         countries.append(country)
         return country, 201
     return {"error": "Request must be JSON"}, 415
+
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
