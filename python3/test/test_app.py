7ab38ddf73732aa23fb69ac08d998855818f18ba
Ivan Santiago <ivsanmendez@gmail.com>
2023-04-20 17:06:34 -0600

    Adding test configuration

---
+++ python3/test/test_app.py
@@ -0,0 +1,12 @@
+import sys
+import unittest
+sys.path.append('../src')
+
+from src import app
+from unittest import TestCase
+
+class AppTest(TestCase):
+    def test_home(self):
+        result = app.home()
+        self.assertEqual(result, "Hello, World!")
+
