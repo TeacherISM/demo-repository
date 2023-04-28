60f5278dc4a88e8c5e6c6c7abf78cc0dc7ba9a34
G3RZZZ <73681300+G3RZZZ@users.noreply.github.com>
2023-04-27 21:02:24 -0600

    Lint changes Gerardo Gutierrez

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,9 +1,10 @@
-import sys
-import unittest
-sys.path.append('../src')
-
 from src import app
 from unittest import TestCase
+
+import sys
+# import unittest
+sys.path.append('../src')
+
 
 class AppTest(TestCase):
     def test_home(self):
