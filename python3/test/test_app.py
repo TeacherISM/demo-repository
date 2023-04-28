fdfa32b8029398ca286d6ba70ae5394b749632f7
karla-mond <karla.mondragon.rosas@gmail.com>
2023-04-27 21:11:15 -0600

    correccion lint flake karla V3

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -5,8 +5,7 @@ sys.path.append('../src')
 
 class AppTest(TestCase):
 
+
     def test_home(self):
         result = app.home()
-
-        self.assertEqual(result, "Hello, World!")
-        
+        self.assertEqual(result,"Hello, World!")
