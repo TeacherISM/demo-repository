0d9378858483717fb5db10dba017e008301f34af
karla-mond <karla.mondragon.rosas@gmail.com>
2023-04-27 21:15:00 -0600

    correccion lint flake 8 karla v4

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -3,9 +3,9 @@ from src import app
 from unittest import TestCase
 sys.path.append('../src')
 
+
 class AppTest(TestCase):
-
-
     def test_home(self):
         result = app.home()
-        self.assertEqual(result,"Hello, World!")
+        self.assertEqual(result, "Hello, World!")
+        
