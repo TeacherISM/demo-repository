36aeb9b75082d340b72179f3c272ef9f0582e8b0
Uriel Aguilar <78172208+u-urieldev@users.noreply.github.com>
2023-05-02 20:41:09 -0600

    fix lint

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,7 +1,5 @@
 import sys
-import unittest
 sys.path.append('../src')
-
 from src import app
 from unittest import TestCase
 
@@ -9,4 +7,3 @@ class AppTest(TestCase):
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
-
