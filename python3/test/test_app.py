07ff030f9075c9303b9b44e2d4f08db0fa777bb8
Joshua Amaya <josrubcam@hotmail.com>
2023-05-02 23:45:43 -0600

    pipe_v0.2

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,12 +1,13 @@
+from src import app
+from unittest import TestCase
 import sys
-import unittest
 sys.path.append('../src')
 
 from src import app
 from unittest import TestCase
 
+
 class AppTest(TestCase):
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
-
