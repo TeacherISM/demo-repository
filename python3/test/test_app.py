60df73d5e903f2a4b2a1431858721b71a054438a
Salvador Salgado <chavasalgado27@gmail.com>
2023-05-02 20:51:54 -0600

    Update test_app.py

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -3,7 +3,9 @@ from src import app
 import sys
 sys.path.append('../src')
 
+
 class AppTest(TestCase):
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
+        
