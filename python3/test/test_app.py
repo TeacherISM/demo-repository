30dfea2a52bc610d9e17a3def6e4175e6b34489c
DiegoAraque21 <diego.araque.fernandez@gmail.com>
2023-05-02 20:39:23 -0600

    .

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -5,9 +5,6 @@ from unittest import TestCase
 sys.path.append('../src')
 
 class AppTest(TestCase):
-
-
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
-
