791699424980d8bb8887ecc85ab7b86e47ed443c
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-05-04 21:47:30 -0600

    Update test_app.py

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -4,20 +4,21 @@ import sys
 
 sys.path.append('../src')
 
+
 class AppTest(TestCase):
-    
+
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
-    
+
     def setUp(self):
         self.app = app.app.test_client()
-    
+
     def test_welcome(self):
         result = self.app.get('/welcome')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Welcome', result.data)
-    
+
     def test_get_countries(self):
         result = self.app.get('/countries')
         self.assertEqual(result.status_code, 200)
@@ -25,13 +26,13 @@ class AppTest(TestCase):
         self.assertIn('Thailand', data)
         self.assertIn('Australia', data)
         self.assertIn('Egypt', data)
-    
+
     def test_add_country(self):
         result = self.app.post('/countries', json={"name": "New Zealand", "capital": "Wellington", "area": 268021})
         self.assertIn(b"New Zealand", result.data)
         self.assertIn(b"Wellington", result.data)
         self.assertIn(b"268021", result.data)
-    
+
     def test_add_country_missing_json(self):
         result = self.app.post('/countries')
         self.assertEqual(result.status_code, 415)
