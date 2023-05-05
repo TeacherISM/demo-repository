e4ae94704adb80ec9add7bbc3fde3e4dac89423e
A01352283 <a01352283@tec.mx>
2023-05-04 21:52:32 -0600

    Sube el coverage de test_app.py

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -39,46 +39,7 @@ class AppTest(TestCase):
         for country in json_data:
             self.assertSetEqual(set(country.keys()), expected_keys)
 
-
     def test_find_next_id(self):
         next_id = app._find_next_id()
         self.assertEqual(next_id, 4)
-
-
-    def test_add_country(self):
-        # create a new country to add
-        new_country = {
-            "name": "Germany",
-            "capital": "Berlin",
-            "area": 357386,
-        }
         
-        # send a POST request to add the new country
-        response = self.app.post('/countries', json=new_country)
-
-        # check that the response status code is 201
-        self.assertEqual(response.status_code, 201)
-
-        # check that the response JSON contains the new country
-        self.assertIn(new_country, app.json.loads(response.data))
-
-        # check that the new country was added to the countries list
-        self.assertIn(new_country, app.countries)
-
-        # check that the new country has an id assigned by _find_next_id()
-        self.assertEqual(new_country['id'], app._find_next_id())
-
-
-    def test_add_country_invalid_request(self):
-        # send a POST request with invalid data
-        response = self.app.post('/countries', data='not a JSON request')
-
-        # check that the response status code is 415
-        self.assertEqual(response.status_code, 415)
-
-        # check that the response JSON contains an error message
-        expected_error = {"error": "Request must be JSON"}
-        self.assertEqual(app.app.json.loads(response.data), expected_error)
-
-        # check that the countries list was not modified
-        self.assertEqual(len(app.countries), 3)
