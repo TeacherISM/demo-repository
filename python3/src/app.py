e0383ff8b73d022abec343fe33d67543e86fc4d8
reginardzss <84197750+reginardzss@users.noreply.github.com>
2023-05-04 20:41:01 -0600

    Update app.py

--- python3/src/app.py
+++ python3/src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # use decorators to link the function to a url
 @app.route('/')
 def home():
-    return "Hola, World!"  # return a string
+    return "Hello, World!"  # return a string
 
 
 @app.route('/welcome')
