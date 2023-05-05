34f0163aac082d29d4e815786e65c818124ef011
Trh7101 <97247188+Trh7101@users.noreply.github.com>
2023-05-04 22:59:17 -0600

    Added Unit tests

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -5,8 +5,29 @@ sys.path.append('../src')
 from src import app
 from unittest import TestCase
 
+
 class AppTest(TestCase):
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
+
+    def setUp(self):
+        self.app = app.app.test_client()
+
+    def test_welcome(self):
+        result = self.app.get('/welcome')
+        self.assertIn(b'Welcome!', result.data)
+
+    def test_countries(self):
+        result = self.app.get('/countries')
+        self.assertIn(b"Thailand", result.data)
+        self.assertIn(b"Australia", result.data)
+        self.assertIn(b"Egypt", result.data)
+
+    def test_add_country(self):
+        result = self.app.post('/countries', json={"name": "Turkey", "capital": "Ankara", "area": 80268021})
+        self.assertIn(b"Turkey", result.data)
+        self.assertIn(b"Ankara", result.data)
+        self.assertIn(b"80268021", result.data)
+
 
