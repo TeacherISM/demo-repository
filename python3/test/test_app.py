52905c9858ed1e710e53323e0fb9f3000207386b
Ana Paula Katsuda <a01025303@tec.mx>
2023-05-04 21:37:52 -0600

    added some tests v1

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -9,29 +9,26 @@ class AppTest(TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
+
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hola, soy akemi")
 
     # test if the response is 200 for the welcome page
     def test_welcome(self):
-        with app.test_client() as client:
-            response = client.get('/welcome')
-            self.assertEqual(response.status_code, 200)
+        response = self.app.get('/welcome')
+        self.assertEqual(response.status_code, 200)
 
     # test if the response is 200 for the countries page
     def test_countries(self):
-        with app.test_client() as client:
-            response = client.get('/countries')
-            self.assertEqual(response.status_code, 200)
-    
+        response = self.app.get('/countries')
+        self.assertEqual(response.status_code, 200)
+
     # test if the response is 201 for the countries page
-    def test_countries(self):
-        with app.test_client() as client:
-            response = client.post('/countries')
-            self.assertEqual(response.status_code, 201)
-    
+    def test_countries_201(self):
+        response = self.app.post('/countries')
+        self.assertEqual(response.status_code, 201)
+
     # test countries get endpoint
     def test_countries_get(self): 
         result = self.app.get('/countries')
@@ -45,7 +42,7 @@ class AppTest(TestCase):
         self.assertIn(b'Cairo', result.data)
         self.assertIn(b'1010408', result.data)
         self.assertEqual(result.status_code, 200)
-    
+
     # test countries post endpoint
     def test_countries_post(self):
         new_country = {"name": "Mexico", "capital": "Mexico City", "area": 1964375}
@@ -57,7 +54,7 @@ class AppTest(TestCase):
     def test_next_id(self):
         result = self.app._find_next_id()
         self.assertEqual(result, 4)
-    
+
     # test invalid post endpoint
     def test_add_country_invalid_request(self):
         result = self.app.post('/countries', data='{"name": "China"}', content_type='application/json')
