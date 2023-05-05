1d4dffa8534552a1d10735c1b3628e4e649c49eb
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-05-04 21:45:28 -0600

    Update test_app.py

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,45 +1,38 @@
 from src import app
-from src.app import countries
 from unittest import TestCase
-from flask import jsonify
 import sys
+
 sys.path.append('../src')
 
 class AppTest(TestCase):
+    
     def test_home(self):
-
         result = app.home()
         self.assertEqual(result, "Hello, World!")
-
+    
     def setUp(self):
-
-      self.app = app.app.test_client()
-
+        self.app = app.app.test_client()
+    
     def test_welcome(self):
-        
         result = self.app.get('/welcome')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Welcome', result.data)
-
+    
     def test_get_countries(self):
-      result = self.app.get('/countries')
-      self.assertEqual(result.status_code, 200)
-      result.data.decode()
-      self.assertIn(b'Thailand', result.data)
-      self.assertIn(b'Australia', result.data)
-      self.assertIn(b'Egypt', result.data)
-
+        result = self.app.get('/countries')
+        self.assertEqual(result.status_code, 200)
+        data = result.data.decode()
+        self.assertIn('Thailand', data)
+        self.assertIn('Australia', data)
+        self.assertIn('Egypt', data)
+    
     def test_add_country(self):
         result = self.app.post('/countries', json={"name": "New Zealand", "capital": "Wellington", "area": 268021})
         self.assertIn(b"New Zealand", result.data)
         self.assertIn(b"Wellington", result.data)
         self.assertIn(b"268021", result.data)
-
+    
     def test_add_country_missing_json(self):
         result = self.app.post('/countries')
         self.assertEqual(result.status_code, 415)
         self.assertIn(b"Request must be JSON", result.data)
-
-
-
-
