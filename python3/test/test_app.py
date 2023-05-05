e0948778026c21a1b6702c440c72a7a14d06b0e5
Ana Paula Katsuda <a01025303@tec.mx>
2023-05-04 21:37:52 -0600

    added some tests v1

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -8,7 +8,7 @@ class AppTest(TestCase):
     def setUp(self):
         self.app = app.test_client()
         self.app.testing = True
-    
+
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
