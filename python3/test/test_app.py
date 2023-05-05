4c1c09bf1fc7c74b7a9fd1a608e2ca557bbb10b1
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-04 21:36:48 -0600

    Finsihed tests

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -1,5 +1,6 @@
 import sys
-from src import app
+from flask.testing import FlaskClient
+from src.app import app
 from unittest import TestCase
 
 sys.path.append("../src")
@@ -7,6 +8,31 @@ sys.path.append("../src")
 
 class AppTest(TestCase):
 
+    def setUp(self):
+        self.client = app.test_client()
+
     def test_home(self):
-        result = app.home()
-        self.assertEqual(result, "Hello, World!")
+        result = self.client.get('/')
+        self.assertEqual(result.data, b"Hello, World!")
+
+    def test_welcome(self):
+        result = self.client.get("/welcome")
+        self.assertIn(b"Welcome!", result.data)
+
+    def test_get_countries(self):
+        result = self.client.get("/countries")
+        self.assertIn(b"Thailand", result.data)
+        self.assertIn(b"Australia", result.data)
+        self.assertIn(b"Egypt", result.data)
+
+    def test_add_country(self):
+        result = self.client.post('/countries', json={"name": "New Zealand", "capital": "Wellington", "area": 268021})
+        self.assertIn(b"New Zealand", result.data)
+        self.assertIn(b"Wellington", result.data)
+        self.assertIn(b"268021", result.data)
+
+    def test_add_country_missing_json(self):
+        result = self.client.post('/countries')
+        self.assertEqual(result.status_code, 415)
+        self.assertIn(b"Request must be JSON", result.data)
+
