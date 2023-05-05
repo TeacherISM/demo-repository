69a34bf1a0a2ff41de7a53931b6a1babde2a3052
Salvador Federico Milanes Braniff <90704785+SFMBa01029956@users.noreply.github.com>
2023-05-04 20:20:03 -0600

    Add additional test functions

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -8,3 +8,23 @@ class AppTest(TestCase):
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
+
+    def test_welcome(self):
+        result = app.welcome()
+        self.assertEqual(result, "Welcome to Flask")
+
+    def find_next_id(self):
+        result = app._find_next_id()
+        self.assertEqual(result, 4)
+
+    def test_get_countries(self):
+        result = app.get_countries()
+        self.assertEqual(result, app.jsonify(app.countries))
+
+    def test_add_country(self):
+        result = app.add_country()
+        self.assertEqual(result, app.country, 201)
+
+    # def test_add_country_error(self):
+
+        
