2e2f5a6f4d2927f81ad56515a301dd2bd539e8ef
Ana Paula Katsuda <a01025303@tec.mx>
2023-05-04 21:44:56 -0600

    added some tests v3

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -6,8 +6,9 @@ sys.path.append('../src')
 
 class AppTest(TestCase):
     def setUp(self):
-        self.app = app.test_client()
+        self.app = app.app.test_client()
         self.app.testing = True
+
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
