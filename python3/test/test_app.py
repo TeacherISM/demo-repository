e7564599b347e4cefb6952d5a6ce687d17d8e724
Luisja <52958375+Luisjakg@users.noreply.github.com>
2023-05-04 20:45:47 -0600

    Update test_app.py

--- python3/test/test_app.py
+++ python3/test/test_app.py
@@ -4,9 +4,7 @@ from unittest import TestCase
 
 sys.path.append("../src")
 
-
 class AppTest(TestCase):
-    
     def test_home(self):
         result = app.home()
         self.assertEqual(result, "Hello, World!")
