292743c772bc382198ba6e9d2f2b74811e1980b0
Ivan Santiago <ivsanmendez@gmail.com>
2023-05-16 21:27:03 -0600

    ecr running

--- terraform/dockering.tf
+++ terraform/dockering.tf
@@ -6,12 +6,11 @@ resource "null_resource" "docker_packaging" {
 
   provisioner "local-exec" {
     command = <<EOF
-    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin ${data.aws_caller_identity.current.account_id}.dkr.ecr.us-east-1.amazonaws.com
-    pwd
-    ls
     cd ../python3
-    docker build -t "${aws_ecrpublic_repository.aws_dev_ecr.repository_uri}/app:latest" -f Dockerfile .
-    docker push "${aws_ecrpublic_repository.aws_dev_ecr.repository_uri}/app:latest"
+    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/d0z3f1c0
+    docker build -t "demo-repository-ecr:latest" -f Dockerfile .
+    docker tag demo-repository-ecr:latest public.ecr.aws/d0z3f1c0/demo-repository-ecr:latest
+    docker push public.ecr.aws/d0z3f1c0/demo-repository-ecr:latest
     EOF
   }
 
