822f37c913119acd0334edc2e5a5fce047fcbe73
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2023-05-19 14:06:47 -0600

    Terraform with cmd

--- terraform/dockering.tf
+++ terraform/dockering.tf
@@ -5,13 +5,9 @@ data "aws_caller_identity" "current" {
 resource "null_resource" "docker_packaging" {
 
   provisioner "local-exec" {
-    command = <<EOF
-    cd ../python3
-    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/d0z3f1c0
-    docker build -t "demo-repository-ecr:latest" -f Dockerfile .
-    docker tag demo-repository-ecr:latest public.ecr.aws/d0z3f1c0/demo-repository-ecr:latest
-    docker push public.ecr.aws/d0z3f1c0/demo-repository-ecr:latest
-    EOF
+  command = <<EOF
+    cd ../python3 && aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/e9m4m2x0 && docker build -t demo-repository-ecr-2:latest -f Dockerfile . && docker tag demo-repository-ecr-2:latest public.ecr.aws/e9m4m2x0/demo-repository-ecr-2:latest && docker push public.ecr.aws/e9m4m2x0/demo-repository-ecr-2:latest
+  EOF
   }
 
   triggers = {
