26ac4f6688bc87efafaec28711ec61662f11ee47
Ivan Santiago <ivsanmendez@gmail.com>
2023-05-23 20:49:07 -0600

    Correcting errors

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,6 +10,7 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   sts_region = "us-east-1"
-  region = "us-east-1"
-  alias = "us_east_1"
+  region     = "us-east-1"
+  alias      = "us_east_1"
+
 }
